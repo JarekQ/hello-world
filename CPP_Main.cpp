2402f80b808369b74b86101959f1304d0a4b4b1f
JarekQ <71729071+jaloisio-incomm@users.noreply.github.com>
2021-07-29 14:05:35 -0400

    Created a new CPP File as a Hello World start

---
+++ CPP_Main.cpp
@@ -0,0 +1,17 @@
+/*-------------
+JarekQ's CPP Main Code
+
+CREATED BY: JarekQ Aloisio
+CREATED ON: 07/29/2021
+UPDATED ON: 07/29/2021
+
+Purpose: To serve as a portfolio program
+to demonstrate aptitude of coding in C++.
+-------------*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World\n";
+}
